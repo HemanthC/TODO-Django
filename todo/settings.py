9df19cf106645313971499a93ac2b80ca79c9a99
Hemanth C <hemanthseervi21821@gmail.com>
2023-03-09 16:27:30 +0530

    removed secret key

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f(mnxk93zce*30%jti9q2wach13ifg($3$b1pomjid+f=qahuq'
+SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
